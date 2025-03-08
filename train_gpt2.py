776888c2b2a3ddb5dd40610b6e7db0aa1158e5f7
Alexandre TL <alextorresleguet@icloud.com>
2025-03-08 13:15:59 +0100

    warmup iters

--- train_gpt2.py
+++ train_gpt2.py
@@ -61,7 +61,7 @@ beta2 = 0.95
 grad_clip = 1.0 # clip gradients at this value, or disable if == 0.0
 # learning rate decay settings
 decay_lr = True # whether to decay the learning rate
-warmup_iters = 2000 # how many steps to warm up for
+warmup_iters = 500 # how many steps to warm up for
 lr_decay_iters = 10_000 # should be ~= max_iters per Chinchilla
 min_lr = 0 # minimum learning rate, should be ~= learning_rate/10 per Chinchilla
 # DDP settings
